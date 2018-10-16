a2996306fc16e5d6d8dc091591bfe13eda0ba5b5
Brad <bradleofabian@gmail.com>
2018-10-15 23:36:23 -0400

    set up componant

---
+++ src/components/Row/Row.js
@@ -0,0 +1,10 @@
+// Import React
+import React from "react";
+
+// Row component
+const Row = props => (
+  <div className={`row${props.fluid ? "-fluid" : ""}`} {...props} />
+);
+
+// export Row component.
+export default Row;
