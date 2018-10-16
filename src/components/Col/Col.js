edf46e9815b137f8ba27829605c84bfc1541e4da
Brad <bradleofabian@gmail.com>
2018-10-15 23:05:11 -0400

    set up cols

---
+++ src/components/Col/Col.js
@@ -0,0 +1,14 @@
+// Import React
+import React from "react";
+
+// Define column component (layout)
+const Col = props => {
+  const size = props.size
+    .split(" ")
+    .map(size => "col-" + size)
+    .join(" ");
+  return <div className={size} {...props} />;
+};
+
+// Export column.
+export default Col;
